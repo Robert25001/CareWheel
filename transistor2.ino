0730a31b61f6f9bf83218ae2ef06e11649f0ae33
jaspy593 <rasoloniainamiarisoa@gmail.com>
2026-07-31 14:51:32 +0300

    Add files via upload

--- transistor2.ino
+++ transistor2.ino
@@ -1,18 +1,34 @@
 #include <WiFi.h>
-const char* ssid = "MIT-Sous-Sol";
+#include <Wire.h>
+#include "MAX30105.h"
+#include "spo2_algorithm.h"
+const char* ssid = "MIT-Grande-Salle";
 const char* password = "!321poiuytreza";
 WiFiServer server(5000);
+WiFiClient client;
+
+MAX30105 particleSensor;
+uint32_t irBuffer[100];
+uint32_t redBuffer[100];
+int32_t bufferLength = 100;
+int32_t spo2;
+int8_t validSPO2;
+int32_t heartRate;
+int8_t validHeartRate;
+
 const int IN1 = 18;
 const int IN2 = 19;
 const int IN3 = 21;
 const int IN4 = 22;
 const int ENA = 23;
 const int ENB = 5;
-const int TRIG = 2;
-const int ECHO = 4;
+const int TRIG = 12;
+const int ECHO = 13;
+unsigned long dernierEnvoi = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+
   WiFi.begin(ssid,password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -24,6 +40,31 @@ void setup() {
   server.begin();
   Serial.println("Server TCP demarre sur le port:");
   Serial.println(5000);
+  
+  Wire.begin(2, 4);
+  Serial.println("Recherche des périphériques I2C...");
+   if (!particleSensor.begin(Wire, I2C_SPEED_FAST))
+   {
+    Serial.println("MAX30102 non detecter");
+    while(1) 
+    {
+      delay(1000);
+    }
+   }
+  Serial.println("MAX30102 detecter");
+  
+  particleSensor.setup(
+    80,
+    4,
+    2,
+    100,
+    411,
+    4096
+  );
+  
+  particleSensor.setPulseAmplitudeRed(0x0A);
+  particleSensor.setPulseAmplitudeIR(0x0A);
+  
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
@@ -38,14 +79,23 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:  digitalWrite(led, LOW);
   connexion();
-  /*avancer();
+  lireCapteur();
+
+  if (millis() - dernierEnvoi >= 2000)
+  {
+    dernierEnvoi = millis();
+
+    envoyerDonnees();
+  }
+  
+  avancer();
   delay(1000);
   arreter();
   delay(3000);
   gauche();
   delay(1000);
   droite();
-  delay(1000);*/
+  delay(1000);
 }
 
 void avancer() {
@@ -112,9 +162,9 @@ float mesure_distance() {
   long temps = pulseIn(ECHO,HIGH);
   float distance = temps/58.0;
 
-  Serial.print("Distance:");
+  /*Serial.print("Distance:");
   Serial.print(distance);
-  Serial.println("cm");
+  Serial.println("cm");*/
 
   return(distance);
 }
@@ -149,27 +199,110 @@ void connexion() {
         String commande = client.readStringUntil('\n');
         commande.trim();
         Serial.println(commande);
-        client.println("Bonjour depuis ESP32");
-        if(commande == "avancer") {
-           evite_obstacle();
-           avancer();
-        }
-        else if(commande == "reculer") {
-           evite_obstacle();
+        execute_commande(commande);
+      }
+    }      
+    client.stop();
+    Serial.println("PC deconnecter");
+  }
+}
+
+void execute_commande(String commande) 
+{
+  
+  if(commande == "avancer") {
+     evite_obstacle();
+     avancer();
+    }
+    else if(commande == "reculer") {
+          evite_obstacle();
            reculer();
         }
         else if(commande == "droite") {
            evite_obstacle();
            droite();
         }
+        else if(commande == "arreter") {
+          evite_obstacle();
+          arreter();
+        }
         else {
            evite_obstacle();
            gauche();
         }
+}
+
+void lireCapteur()
+{
+  static bool nouveauBloc = true;
+
+  if (nouveauBloc)
+  {
+    for (int i = 0; i < bufferLength; i++)
+    {
+      while (particleSensor.available() == false)
+      {
+        particleSensor.check();
       }
+
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+
+      particleSensor.nextSample();
     }
-    client.stop();
-    Serial.println("PC deconnecter");
+
+    maxim_heart_rate_and_oxygen_saturation(
+      irBuffer,
+      bufferLength,
+      redBuffer,
+      &spo2,
+      &validSPO2,
+      &heartRate,
+      &validHeartRate
+    );
+
+    nouveauBloc = false;
+
+    Serial.print("BPM : ");
+
+    if (validHeartRate)
+      Serial.println(heartRate);
+    else
+      Serial.println("--");
+
+    Serial.print("SpO2 : ");
+
+    if (validSPO2)
+      Serial.println(spo2);
+    else
+      Serial.println("--");
+
+    Serial.println("----------------");
+
+    nouveauBloc = true;
   }
 }
 
+void envoyerDonnees()
+{
+  if (!client || !client.connected())
+    return;
+
+  client.print("BPM:");
+
+  if (validHeartRate)
+    client.print(heartRate);
+  else
+    client.print("--");
+
+  client.print(";SPO2:");
+
+  if (validSPO2)
+    client.print(spo2);
+  else
+    client.print("--");
+
+  client.println();
+
+  Serial.println("Donnees envoyees a Qt.");
+}
